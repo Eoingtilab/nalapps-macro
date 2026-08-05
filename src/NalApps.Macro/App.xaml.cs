0ba73f4859c263dafbf37492d034601ad6d1305b
Eoingti Lab Inc. <master@eoingti.com>
2026-08-05 10:36:57 +0900

    Keep utility open on recoverable UI exceptions

--- src/NalApps.Macro/App.xaml.cs
+++ src/NalApps.Macro/App.xaml.cs
@@ -1,7 +1,44 @@
 using System.Windows;
+using System.Windows.Threading;
+using NalApps.Macro.Core;
 
 namespace NalApps.Macro;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindowApplyGuard.Register();
+        DispatcherUnhandledException += HandleDispatcherUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
+        TaskScheduler.UnobservedTaskException += HandleUnobservedTaskException;
+        base.OnStartup(e);
+    }
+
+    private static void HandleDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        CrashReporter.Write("DispatcherUnhandledException", e.Exception);
+        MessageBox.Show(
+            "예기치 않은 오류가 발생했지만 프로그램 종료를 막았습니다.\n\n" +
+            e.Exception.Message +
+            "\n\n오류 기록: " + CrashReporter.LogPath,
+            "날라앱스 매크로 오류",
+            MessageBoxButton.OK,
+            MessageBoxImage.Error);
+        e.Handled = true;
+    }
+
+    private static void HandleUnhandledException(object? sender, UnhandledExceptionEventArgs e)
+    {
+        if (e.ExceptionObject is Exception exception)
+        {
+            CrashReporter.Write("AppDomain.UnhandledException", exception);
+        }
+    }
+
+    private static void HandleUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+    {
+        CrashReporter.Write("TaskScheduler.UnobservedTaskException", e.Exception);
+        e.SetObserved();
+    }
 }
